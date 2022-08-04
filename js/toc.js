80f8a39dd6d1de4e7add46f8065697bc60bb581e
dbc-hou <david.collins.hou@gmail.com>
2022-08-03 21:14:40 -0500

    began moving table of contents content to toc.js

--- js/toc.js
+++ js/toc.js
@@ -32,6 +32,26 @@ function volILinks() {
       <li id="lesson10" class="list-group-item pl-2 chapter"><a id="lesson-x-link" class="chapter-link" href="LessonX.html">X. Neuter Nouns</a></li>
       <li id="lesson11" class="list-group-item pl-2 chapter"><a id="lesson-xi-link" class="chapter-link" href="LessonXI.html">XI. Indirect Objects</a></li>
       <li id="lesson12" class="list-group-item pl-2 chapter"><a id="lesson-xii-link" class="chapter-link" href="LessonXII.html">XII. Review I</a></li>
-</ul>
-</li>`
+    </ul>
+  </li>`
+}
+
+function volIILinks() {
+  return `<li class="list-group-item volume"><a id="vol-ii" class="volume-link" href="#" onclick="$('#vol-ii-parts').slideToggle('slow')" style="color: black" onmouseover="this.css('color','gold')">Volūmen II</a>
+    <ul id="vol-ii-parts" class="list-group sublist">
+      <li id="ii-prae" class="list-group-item pl-2 chapter"><a id="vol-i-prae-link" class="chapter-link" href="PraeludiumII.html">Praelūdium II</a></li>
+      <li id="lesson13" class="list-group-item pl-2 chapter"><a id="lesson-xiii-link" class="chapter-link" href="LessonXIII.html">XIII. Accompaniment</a></li>
+      <li id="lesson14" class="list-group-item pl-2 chapter"><a id="lesson-xiv-link" class="chapter-link" href="LessonXIV.html">XIV. Ego et Tū, Brūte</a></li>
+      <li id="lesson15" class="list-group-item pl-2 chapter"><a id="lesson-xv-link" class="chapter-link" href="LessonXV.html">XV. It Takes a Dative</a></li>
+      <li id="lesson16" class="list-group-item pl-2 chapter"><a id="lesson-xvi-link" class="chapter-link" href="LessonXVI.html">XVI. Nōs Vōbīs Imperāmus!</a></li>
+      <li id="lesson17" class="list-group-item pl-2 chapter"><a id="lesson-xvii-link" class="chapter-link" href="LessonXVII.html">XVII. Preposition Power</a></li>
+      <li id="lesson18" class="list-group-item pl-2 chapter"><a id="lesson-xviii-link" class="chapter-link" href="LessonXVII.html">XVIII. Sibi Sunt Benīgnī</a></li>
+      <li id="lesson19" class="list-group-item pl-2 chapter"><a id="lesson-xix-link" class="chapter-link" href="LessonXIX.html">XIX. Fun with Infinitives</a></li>
+      <li id="lesson20" class="list-group-item pl-2 chapter"><a id="lesson-xx-link" class="chapter-link" href="LessonXX.html">XX. Imperfect Tense</a></li>
+      <li id="lesson21" class="list-group-item pl-2 chapter"><a id="lesson-xxi-link" class="chapter-link" href="LessonXXI.html">XXI. Future Tense</a></li>
+      <li id="lesson22" class="list-group-item pl-2 chapter"><a id="lesson-xxii-link" class="chapter-link" href="LessonXXII.html">XXII. Irregular Imperfect and Future</a></li>
+      <li id="lesson23" class="list-group-item pl-2 chapter"><a id="lesson-xxiii-link" class="chapter-link" href="LessonXXIII.html">XXIII. Passive Voice Endings</a></li>
+      <li id="lesson24" class="list-group-item pl-2 chapter"><a id="lesson-xxiv-link" class="chapter-link" href="LessonXXIV.html">XXIV. Review II</a></li>
+    </ul>
+  </li>`
 }
