10a78439f32c25647fb364e4ba0a665e6a9f9ea3
David B. Collins <david.collins.hou@gmail.com>
2022-08-29 15:11:30 -0500

    Review III complete

--- js/toc.js
+++ js/toc.js
@@ -11,8 +11,8 @@ function tableOfContents() {
 }
 
 function introLinks() {
-  return `<li class="list-group-item volume"><a id="intro" class="volume-link" href="#" onclick="$('#intro-parts').slideToggle('slow')" style="color: black" onmouseover="this.css('color','gold')">Introduction</a>
-  <ul id="intro-parts" class="list-group sublist">
+  return `<div id="intro-div"><li class="list-group-item volume"><a id="intro" class="volume-link" href="#" onclick="$('#intro-parts').slideToggle('slow')" style="color: black" onmouseover="this.css('color','gold')">Introduction</a>
+  <div id="intro-list-div" style="overflow-y: scroll"><ul id="intro-parts" class="list-group sublist">
     <li id="intro-prae" class="list-group-item pl-2 chapter"><a id="intro-prae-link" class="chapter-link" href="IntroPrae.html">Praelūdium</a></li>
     <li id="intro-a" class="list-group-item pl-2 chapter"><a id="intro-a-link" class="chapter-link" href="IntroA.html">A. Why Latin?</strong></li>
     <li id="intro-b" class="list-group-item pl-2 chapter"><a id="intro-b-link" class="chapter-link" href="IntroB.html">B. Evolution of Latin</a></li>
@@ -23,13 +23,13 @@ function introLinks() {
     <li id="intro-g" class="list-group-item pl-2 chapter"><a id="intro-g-link" class="chapter-link" href="IntroG.html">G. Months of the Year</a></li>
     <li id="intro-h" class="list-group-item pl-2 chapter"><a id="intro-h-link" class="chapter-link" href="IntroH.html">H. Days of the Week</a></li>
     <li id="intro-i" class="list-group-item pl-2 chapter"><a id="intro-i-link" class="chapter-link" href="IntroI.html">I. Easy Nouns</a></li>
-  </ul>
-</li>`
+  </ul></div>
+</li></div>`
 }
 
 function volILinks() {
-  return `<li class="list-group-item volume"><a id="vol-i" class="volume-link" href="#" onclick="$('#vol-i-parts').slideToggle('slow')" style="color: black" onmouseover="this.css('color','gold')">Volūmen I</a>
-    <ul id="vol-i-parts" class="list-group sublist">
+  return `<div id="vol-i-div"><li class="list-group-item volume"><a id="vol-i" class="volume-link" href="#" onclick="$('#vol-i-parts').slideToggle('slow')" style="color: black" onmouseover="this.css('color','gold')">Volūmen I</a>
+    <div id="vol-i-list-div" style="overflow-y: auto"><ul id="vol-i-parts" class="list-group sublist">
       <li id="i-prae" class="list-group-item pl-2 chapter"><a id="vol-i-prae-link" class="chapter-link" href="PraeludiumI.html">Praelūdium I</a></li>
       <li id="lesson01" class="list-group-item pl-2 chapter"><a id="lesson-i-link" class="chapter-link" href="LessonI.html">I. Famīliae Rōmānae</a></li>
       <li id="lesson02" class="list-group-item pl-2 chapter"><a id="lesson-ii-link" class="chapter-link" href="LessonII.html">II. Forms of Address</a></li>
@@ -43,8 +43,8 @@ function volILinks() {
       <li id="lesson10" class="list-group-item pl-2 chapter"><a id="lesson-x-link" class="chapter-link" href="LessonX.html">X. Neuter Nouns</a></li>
       <li id="lesson11" class="list-group-item pl-2 chapter"><a id="lesson-xi-link" class="chapter-link" href="LessonXI.html">XI. Indirect Objects</a></li>
       <li id="lesson12" class="list-group-item pl-2 chapter"><a id="lesson-xii-link" class="chapter-link" href="LessonXII.html">XII. Recēnsus I</a></li>
-    </ul>
-  </li>`
+    </ul></div>
+  </li></div>`
 }
 
 function volIILinks() {
